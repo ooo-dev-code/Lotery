038c9bd128a1c9598c2c098ff4f71ddd8cf14f03
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2024-10-19 01:35:58 +0200

    Update Random_system.py

--- Random_system.py
+++ Random_system.py
@@ -1,12 +1,14 @@
 from Loto import *
 import random
 
+# Dictionary of possibles values
 result = {
     "value1":None,
     "value2":None,
     "value3":None,
 }
-   
+
+# List of the numbers
 nums = list(range(1, 4))
 random.shuffle(nums)
 
