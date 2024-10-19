cb4ba8e856b5c85a4e3cd2adc5a79f515b3db5e8
ooo <183236447+ooo-dev-code@users.noreply.github.com>
2024-10-20 00:19:29 +0200

    Update Values.py

--- Values.py
+++ Values.py
@@ -1,20 +1,23 @@
 from Loto import *
 import random
 
+# The values are in another file so the code will be more redable and faster to make
 
+# Dimension of the window / Canvas
 width = 600
 height = 600
 
 canvas_width = width-25
 canvas_height = height-25
 
-
+# The values for all the emplacements
 result = {
     "value1":None,
     "value2":None,
     "value3":None,
 }
 
+# Positions and colors of the place for the random machine ( I wanted the simplify at the maximum the drawing of the machine and his system to do it faster )
 place1 = {
     "x1": canvas_width+(25-canvas_width)*(3/3)+10,
     "y1": canvas_height/2+100-10,
@@ -36,6 +39,7 @@ place3 = {
     "y2": canvas_height/2-100+10
 }
 
+# Color for the random machine
 color = {
     1: "red",
     2: "orange",
